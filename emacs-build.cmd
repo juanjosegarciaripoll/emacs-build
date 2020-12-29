7f52a9ee7411ac38c9b36be257ddd6a91eed4e7b
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2020-12-29 17:56:45 +0100

    Use my default build options when no option is passed to emacs-build.cmd

--- emacs-build.cmd
+++ emacs-build.cmd
@@ -1,8 +1,13 @@
 @echo off
+if x%1 == x goto default
 if %1==--help goto help
 if %1==-h goto help
 if %1==-? goto help
 goto run
+
+:default
+emacs-build.cmd --clone --deps --build --pack-emacs --pdf-tools --mu --isync --hunspell --pack-all
+goto:eof
 
 :help
 type %~dp0\scripts\help.txt
