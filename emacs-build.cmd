da6f890203432942279da3c8f4a066106f0e7204
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2020-12-30 15:46:54 +0100

    Handle --clean[-all] directly in emacs-build.cmd

--- emacs-build.cmd
+++ emacs-build.cmd
@@ -1,9 +1,19 @@
 @echo off
 if x%1 == x goto default
+if %1 == --clean goto clean
+if %1 == --clean-all goto cleanall
 if %1==--help goto help
 if %1==-h goto help
 if %1==-? goto help
 goto run
+
+:cleanall
+cd %~dp0 && for %%i in (git build zips pkg) do if exist %%i rmdir /S /Q %%i
+goto:eof
+
+:clean
+cd %~dp0 && for %%i in (build pkg) do if exist %%i rmdir /S /Q %%i
+goto:eof
 
 :default
 emacs-build.cmd --clone --deps --build --pack-emacs --pdf-tools --mu --isync --hunspell --pack-all
