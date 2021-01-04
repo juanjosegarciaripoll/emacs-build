1c98bfa53b8130fd2ea7f439d7cb36ad86bbea50
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2021-01-04 18:09:16 +0100

    Add the MSYS2 emacs-lisp directories and set TMPDIR for MSYS2 commands.

--- scripts/site-start.el
+++ scripts/site-start.el
@@ -3,4 +3,10 @@
        (emacs-root (substring emacs-libexec-path 0 emacs-libexec-ndx)))
   (add-to-list 'exec-path (expand-file-name "bin/" emacs-root))
   (add-to-list 'exec-path (expand-file-name "usr/bin/" emacs-root))
-  (add-to-list 'load-path (expand-file-name "usr/share/emacs/site-lisp/" emacs-root)))
+  (let ((default-directory (expand-file-name "usr/share/emacs/site-lisp/" emacs-root)))
+    (when (file-exists-p default-directory)
+      (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+	  (normal-top-level-add-subdirs-to-load-path)))))
+
+;; This is for MSYS2 tools
+(setenv "TMPDIR" (getenv "TEMP"))
