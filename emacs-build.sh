9eae29478e44ddfcbb596e04441d941d6973919c
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2021-05-16 19:13:38 +0200

    Use NATIVE_FULL_AOT for Emacs build with native-compilation

--- emacs-build.sh
+++ emacs-build.sh
@@ -408,7 +408,8 @@ while test -n "$*"; do
         --with-all) add_all_features;;
         --without-*) delete_feature `echo $1 | sed -e 's,--without-,,'`;;
         --with-*) add_feature `echo $1 | sed -e 's,--without-,,'`;;
-        --nativecomp) add_feature native-compilation;;
+        --nativecomp) add_feature native-compilation
+					  export NATIVE_FULL_AOT=1;;
         --slim) add_all_features
                 delete_feature cairo # We delete features here, so that user can repopulate them
                 delete_feature rsvg
